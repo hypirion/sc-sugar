032fa953b7f0ab128ff2a2ae4f5bb3e412f05a41
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-01-04 15:20:59 +0100

    Update project and add dependencies.

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject com.hypirion/sc-sugar "0.1.0-SNAPSHOT"
-  :description "TODO: Add in description."
+  :description "(Temporary) sugar macros for simple-check."
   :url "https://github.com/hyPiRion/sc-sugar"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [reiddraper/simple-check "0.5.3"]]
   :profiles {:dev {:plugins [[codox "0.6.4"]]
                    :codox {:output-dir "codox"}}}
   :deploy-branches ["stable"])
