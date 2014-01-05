58e3271f1f43834e60503f289aac234f4dfd9574
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-01-05 13:19:55 +0100

    Implement binding-deps.

--- src/com/hypirion/sc_sugar/sugar.clj
+++ src/com/hypirion/sc_sugar/sugar.clj
@@ -1,9 +1,16 @@
 (ns com.hypirion.sc-sugar.sugar
   (:require [simple-check.generators :as gen]
             [simple-check.properties :as prop]
-            [clojure.walk :as walk]))
+            [clojure.walk :as walk]
+            [clojure.set :as set]))
 
 (defn- all-atoms
   "Returns a set of all the atoms (non-compound values) in the form."
   [form]
   (walk/postwalk (fn [n] (if (coll? n) (reduce into #{} n) #{n})) form))
+
+(defn- binding-deps
+  "Returns all deps which this expression depends on. Ignores binding forms."
+  [prev-vars expr]
+  (-> (all-atoms expr)
+      (set/intersection (set prev-vars))))
